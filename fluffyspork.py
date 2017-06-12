3016fdde2259236fbf79c47c3e688212ccda1ffc
not7CD <norbertszulc13@gmail.com>
2017-06-12 20:36:03 +0200

    db add files

--- fluffyspork.py
+++ fluffyspork.py
@@ -10,36 +10,41 @@ PICTURE_RE = re.compile(r'.*\.jpg$', re.IGNORECASE)
 def process_directory(db, path, fun, recursive=False):
     """ Process all elements in directory, """
 
-    print(path)
     if not os.path.isdir(path):
         print(path, 'is not a directory!')
     else:
         for element in os.listdir(path):
             element_path = os.path.join(path, element)
             if os.path.isdir(element_path) and recursive:
-                process_directory(element_path, fun, recursive)
+                process_directory(db, element_path, fun, recursive)
 
-            elif os.path.isfile(element_path):
+            elif os.path.isfile(element_path) and re.match(PICTURE_RE, element_path):
                 fun(db, element_path)
 
 
 def lambda_add_path(db, path):
+    tail = os.path.basename(path)
+    cursor = db.documents.find_one({"file_name": tail})
+    # print(cursor)
+    if cursor is not None:
+        print('skip: %s' % (tail))
+    else:
 
-    result = db.documentd.insert_one(
-        {
-            "employee": {"id": None,
-                         "unit": None,
-                         "job_title": None},
-            "steps": {
-                "raw": {
-                    "path": path,
-                    "date": datetime.now()
+        result = db.documents.insert_one(
+            {
+                "file_name": tail,
+                "employee": {"id": None,
+                             "unit": None,
+                             "job_title": None},
+                "steps": {
+                    "raw": {
+                        "path": path,
+                        "date": datetime.now()
+                    }
                 }
-            },
-            "name": "Vella",
-            "restaurant_id": "41704620"
-        }
-    )
+            }
+        )
+        print('created %s with %s' % (tail, result.inserted_id))
 
 
 def add_dir_db(db, path):
